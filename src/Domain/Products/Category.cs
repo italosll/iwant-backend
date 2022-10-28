a5809caea8d63e59606881bc4662d5c1f0952f5e
italosll <italomsilva.if@gmail.com>
2022-10-28 18:08:50 -0300

    feat: add product domain

---
+++ src/Domain/Products/Category.cs
@@ -0,0 +1,6 @@
+﻿namespace iwant_backend.Domain.Products;
+
+public class Category : Entity
+{
+    public string Name { get; set; }
+}
---
+++ src/Domain/Products/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace iwant_backend.Domain.Products;
+
+public class Product : Entity
+{
+    public string Name { get; set; }
+    public Category Category { get; set; }
+    public String Description { get; set; }
+    public int Amount { get; set; }
+    public float Price { get; set; }
+
+}
