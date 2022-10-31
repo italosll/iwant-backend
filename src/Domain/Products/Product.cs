35b35c0c33acfb7c39f68e5b097c66555142e7c4
italosll <italomsilva.if@gmail.com>
2022-10-31 07:06:52 -0300

    feat: creating entity framework context

--- src/Domain/Products/Product.cs
+++ src/Domain/Products/Product.cs
@@ -3,6 +3,7 @@
 public class Product : Entity
 {
     public string Name { get; set; }
+    public int CategoryId { get; set; }
     public Category Category { get; set; }
     public String Description { get; set; }
     public int Amount { get; set; }
