216b21d65af41765b81563d49a2bfdef3c58f0bc
italosll <italomsilva.if@gmail.com>
2022-11-18 15:04:07 -0300

    feat: refactor dto classes to records

--- src/Endpoints/Categories/CategoryRequest.cs
+++ src/Endpoints/Categories/CategoryRequest.cs
@@ -1,8 +1,3 @@
 ﻿namespace iwant_backend.Endpoints.Categories;
 
-public class CategoryRequest
-{
-
-    public string Name { get; set; }
-    public bool Active { get; set; }
-}
+public record CategoryRequest(string Name, bool Active);
--- src/Endpoints/Categories/CategoryResponse.cs
+++ src/Endpoints/Categories/CategoryResponse.cs
@@ -1,9 +1,3 @@
 ﻿namespace iwant_backend.Endpoints.Categories;
 
-public class CategoryResponse
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public bool Active { get; set; }
-
-}
+public record CategoryResponse(Guid Id, string Name, bool Active);
